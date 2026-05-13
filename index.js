b4fb9b6a371d20fe64888699fd2613e85c4d585a
nahidvai2009nhotp-wq <nahidvai2009nhotp@gmail.com>
2026-05-13 22:53:32 +0600

    Update index.js

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ app.listen(PORT, () => console.log(`Server listening on port ${PORT}`));
 
 // --- CONFIG ---
 const TOKEN = '8808551290:AAE1sfjmD3PINgBltV5jyNzi7t9kS2lHp7U';
-const ADMIN_ID = 7366391050;
+const ADMIN_ID = 7488161246;
 
 // UPDATED NEXA CONFIG
 const NEXA_API_KEY = 'nxa_c9b7b9961da8c469f9cecfe7c78518b01655d1cd';
